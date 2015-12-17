d207a0d7059ad6e0dba41c41e8ddcfc246658307
Kokka <genericoval@gmail.com>
2015-12-17 13:38:20 +0100

    added Click-Dummies for Event(s)

--- app/models/Event.java
+++ app/models/Event.java
@@ -8,8 +8,10 @@ import javax.persistence.Id;
 public class Event extends Model
 {
     @Id
-    public String id;
+    public Long id;
     public String eventname;
     public String date;
 	public String time;
+	
+	public static Finder<Long, Event> find = new Finder<Long, Event>(Event.class);
 }
