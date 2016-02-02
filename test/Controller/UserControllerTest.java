9bd03a318a8b637a03fba38b6c9f6c240cca00cf
rennersebastian <renner.sebastian@gmail.com>
2016-02-02 15:05:29 +0100

    Fixed team model test

--- test/Controller/UserControllerTest.java
+++ test/Controller/UserControllerTest.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import controllers.routes;
 import models.User;
 import org.apache.http.protocol.HTTP;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import play.api.mvc.Session;
+import play.api.test.FakeApplication;
 import play.api.test.FakeRequest;
 import play.libs.Json;
 import play.mvc.Http;
@@ -17,20 +20,28 @@ import play.test.WithApplication;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.running;
+import static play.test.Helpers.*;
 
 /**
  * Created by Sebastian on 01.02.2016.
  */
 public class UserControllerTest extends WithApplication {
+    private static play.test.FakeApplication fakeApplication;
+
+    @BeforeClass
+    public static void startFakeApplication() {
+        fakeApplication = fakeApplication();
+        start(fakeApplication);
+    }
+
+    @AfterClass
+    public static void shutdownFakeApplication() {
+        stop(fakeApplication);
+    }
+
     @Test
     public void testIndex() {
-        running(fakeApplication(), new Runnable() {
-            public void run() {
-                play.mvc.Result result = Helpers.route(routes.Application.index());
-                assertEquals(200, result.status());
-            }
-        });
+        play.mvc.Result result = Helpers.route(routes.Application.index());
+        assertEquals(200, result.status());
     }
 }
